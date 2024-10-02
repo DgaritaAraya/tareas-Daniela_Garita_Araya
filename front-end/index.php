434082582da999144239da24db0157ff4dbdd039
DPlataformas Abiertas <garitaarayadaniela@gmail.com>
2024-10-01 20:48:56 -0600

    Mi primer commit

--- front-end/index.php
+++ front-end/index.php
@@ -1,6 +1,6 @@
 <?php
-    echo "<h1>Practica #1 - Desarrollo con Plataformas abiertas</h1>";
+    echo "<h1>Práctica #1 - Desarrollo con Plataformas abiertas</h1>";
 
-    echo "Comprobando que crear y clonar el repositorio sí funcionó (:"
+    echo "Comprobando que crear y clonar el repositorio sí funcionó (:";
 
 ?>
